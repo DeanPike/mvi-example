ace4f58683a75a60ce21a40816f65556738c1c44
Dean Pike <pike.dean@gmail.com>
2024-03-24 17:41:44 +1100

    Renamed client module to data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 }
 
 dependencies {
-    implementation(project(":client"))
 
+    implementation(project(":data"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
