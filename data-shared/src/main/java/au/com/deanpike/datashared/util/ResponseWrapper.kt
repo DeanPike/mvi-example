ea3e7f1a0ce18b927f96e79f4a7647dbb3afdc35
Dean Pike <pike.dean@gmail.com>
2024-04-01 16:22:55 +1100

    Added response wrapper

---
+++ data-shared/src/main/java/au/com/deanpike/datashared/util/ResponseWrapper.kt
@@ -0,0 +1,6 @@
+package au.com.deanpike.datashared.util
+
+sealed class ResponseWrapper<out T> {
+    data class Success<out T>(val data: T) : ResponseWrapper<T>()
+    data class Error<out T>(val exception: java.lang.Exception) : ResponseWrapper<T>()
+}
