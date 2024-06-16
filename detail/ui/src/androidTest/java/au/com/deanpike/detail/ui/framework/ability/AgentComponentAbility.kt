ce2252eb706870040bed6ee2073428a921538aac
Dean Pike <pike.dean@gmail.com>
2024-06-17 08:09:16 +1000

    Added agent component

---
+++ detail/ui/src/androidTest/java/au/com/deanpike/detail/ui/framework/ability/AgentComponentAbility.kt
@@ -0,0 +1,71 @@
+package au.com.deanpike.detail.ui.framework.ability
+
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_CARD
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_EMAIL
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_FAX
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_GENERAL
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_IMAGE
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_MOBILE
+import au.com.deanpike.detail.ui.shared.AgentComponentTestTags.AGENT_NAME
+import au.com.deanpike.uitestshared.util.assertTagDisplayed
+import au.com.deanpike.uitestshared.util.assertTextDisplayed
+import au.com.deanpike.uitestshared.util.scrollTo
+
+class AgentComponentAbility(private val composeTestRule: ComposeContentTestRule) {
+    fun scrollToAgent(position: Int) {
+        composeTestRule.scrollTo("${AGENT_CARD}_$position")
+    }
+
+    fun assertAgentCardDisplayed(position: Int) {
+        composeTestRule.assertTagDisplayed("${AGENT_CARD}_$position")
+    }
+
+    fun assertAgentNameDisplayed(
+        position: Int,
+        name: String
+    ) {
+        composeTestRule.assertTextDisplayed(
+            tag = "${AGENT_NAME}_$position",
+            text = name
+        )
+    }
+
+    fun assertAgentImageDisplayed(
+        position: Int
+    ) {
+        composeTestRule.assertTagDisplayed("${AGENT_IMAGE}_$position")
+    }
+
+    fun assertAgentMobileContactDisplayed(
+        position: Int,
+        number: String
+    ) {
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_MOBILE}_${position}_LABEL", text = "Mobile")
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_MOBILE}_${position}_VALUE", text = number)
+    }
+
+    fun assertAgentGeneralContactDisplayed(
+        position: Int,
+        number: String
+    ) {
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_GENERAL}_${position}_LABEL", text = "General")
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_GENERAL}_${position}_VALUE", text = number)
+    }
+
+    fun assertAgentFaxContactDisplayed(
+        position: Int,
+        number: String
+    ) {
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_FAX}_${position}_LABEL", text = "Fax")
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_FAX}_${position}_VALUE", text = number)
+    }
+
+    fun assertAgentEmailContactDisplayed(
+        position: Int,
+        email: String
+    ) {
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_EMAIL}_${position}_LABEL", text = "Email")
+        composeTestRule.assertTextDisplayed(tag = "${AGENT_EMAIL}_${position}_VALUE", text = email)
+    }
+}
---
+++ detail/ui/src/main/java/au/com/deanpike/detail/ui/shared/ContactComponent.kt
@@ -0,0 +1,59 @@
+package au.com.deanpike.detail.ui.shared
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import au.com.deanpike.uishared.theme.Dimension.DIM_4
+import au.com.deanpike.uishared.theme.Dimension.DIM_8
+import au.com.deanpike.uishared.theme.MviExampleTheme
+
+@Composable
+fun ContactComponent(
+    modifier: Modifier = Modifier,
+    label: String,
+    number: String,
+    testTag: String
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = DIM_8, end = DIM_4)
+    ) {
+        Text(
+            modifier = Modifier
+                .defaultMinSize(minWidth = 55.dp)
+                .testTag("${testTag}_LABEL"),
+            text = label,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = DIM_4)
+                .testTag("${testTag}_VALUE"),
+            text = number,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactComponentPreview() {
+    MviExampleTheme {
+        ContactComponent(
+            label = "Label",
+            number = "Number",
+            testTag = "TestTag"
+        )
+    }
+}
