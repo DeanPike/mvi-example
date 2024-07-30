76a1403d637ac9baa400291630110ed4c04665cb
Dean Pike <pike.dean@gmail.com>
2024-07-30 18:04:39 +1000

    Lock the screen in portrait for small screens

--- app/src/main/java/au/com/deanpike/mviexample/ui/activity/MainActivity.kt
+++ app/src/main/java/au/com/deanpike/mviexample/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package au.com.deanpike.mviexample.ui.activity
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.window.core.layout.WindowSizeClass
 import androidx.window.core.layout.WindowWidthSizeClass
@@ -19,9 +21,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val activity = this
 
         setContent {
             val windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+
+            DisposableEffect(windowSizeClass) {
+
+                if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.COMPACT) {
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                } else {
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                }
+
+                onDispose {
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                }
+            }
 
             MviExampleTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
