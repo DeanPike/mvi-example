33999530face9406db12f4e146d439f9641358f7
dean <pike.dean@gmail.com>
2026-07-23 21:01:56 +1000

    Added resource shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,8 @@ android {
             isDebuggable = true
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
