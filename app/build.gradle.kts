8306224ef5974bdd6bf6c83576e1c67457bc1378
dean <pike.dean@gmail.com>
2026-07-28 20:48:05 +1000

    Updated version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = getCustomVersionCode()
-        versionName = "2.4.2"
+        versionName = "2.4.3"
 
         vectorDrawables {
             useSupportLibrary = true
